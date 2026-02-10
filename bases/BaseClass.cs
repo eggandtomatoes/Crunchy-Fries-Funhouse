a8126ab5d732d8dbee5a4a0aada0f1e226947efa
Kevin Li <kevinli20100202@163.com>
2026-02-11 02:18:45 +0800

    Add files via upload

---
+++ bases/BaseClass.cs
@@ -0,0 +1,25 @@
+﻿using Exiled.API.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Next_generationSite_27.UnionP
+{
+    public abstract class BaseClass
+    {
+        public void StartInit()
+        {
+            Log.Info($"initing {this.GetType().FullName}");
+            this.Init();
+        }
+        public void StartDelete()
+        {
+            Log.Info($"deleting {this.GetType().FullName}");
+            this.Delete();
+        }
+        abstract public void Init();
+        abstract public void Delete();
+    }
+}
